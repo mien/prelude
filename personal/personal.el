9d465b75a31a25b0c0296990fde2a74c5457b195
Munawwar Hussain Shelia <munwwarhussain@gmail.com>
2018-06-07 12:51:05 +0530

    added more config

--- personal/personal.el
+++ personal/personal.el
@@ -33,7 +33,7 @@
 
 (prelude-require-package 'neotree)
 (global-set-key [f8] 'neotree-toggle)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+;;(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
 (setq python-shell-interpreter "ipython" python-shell-interpreter-args "--simple-prompt -i")
 
@@ -83,9 +83,333 @@
 
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline org-todays-task-file-location "Tasks")
-         "* TODO %?\nEntered on %T"\n)
+         "* TODO %?\nEntered on %T\n")
         ("j" "Journal" entry (file+datetree org-journal-file-location)
-         "* %?\nEntered on %T")
+         "* %?\nEntered on %T\n")
         ("b" "Blog Idea" entry (file+headline org-blog-idea-file-location "ELABORATE")
          "\n* TODO %?\nEntered on %T\n")
         ))
+
+
+;; org-babel settings
+(require 'ob-python)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)))
+(setq org-babel-python-command "python3")
+
+;;----------------- Email setup ------------------------
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+(require 'mu4e)
+`1m
+(setq mu4e-contexts
+      `( ,(make-mu4e-context
+           :name "Gmail"
+           :match-func (lambda (msg) (when msg
+                                       (string-prefix-p "/Gmail" (mu4e-message-field msg :maildir))))
+           :vars '(
+                   (mu4e-trash-folder . "/Gmail/[Gmail].Trash")
+                   (mu4e-refile-folder . "/Gmail/[Gmail].Archive")
+                   ))
+         ))
+
+
+;; I have my "default" parameters from Gmail
+(setq mu4e-sent-folder "/sent"
+      ;; mu4e-sent-messages-behavior 'delete ;; Unsure how this should be configured
+      mu4e-drafts-folder "/drafts"
+      user-mail-address "munawwar.shelia@accionlabs.com"
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587)
+
+;; Now I set a list of
+(defvar my-mu4e-account-alist
+  '(("Gmail"
+     (mu4e-sent-folder "/Gmail/sent")
+     (user-mail-address "munawwar.shelia@accionlabs.com")
+     (smtpmail-smtp-user "munawwar.shelia")
+     (smtpmail-local-domain "gmail.com")
+     (smtpmail-default-smtp-server "smtp.gmail.com")
+     (smtpmail-smtp-server "smtp.gmail.com")
+     (smtpmail-smtp-service 587)
+     )
+     ;; Include any other accounts here ...
+    ))
+
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message.
+   This function is taken from:
+     https://www.djcbsoftware.nl/code/mu/mu4e/Multiple-accounts.html"
+  (let* ((account
+    (if mu4e-compose-parent-message
+        (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+    (string-match "/\\(.*?\\)/" maildir)
+    (match-string 1 maildir))
+      (completing-read (format "Compose with account: (%s) "
+             (mapconcat #'(lambda (var) (car var))
+            my-mu4e-account-alist "/"))
+           (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+           nil t nil nil (caar my-mu4e-account-alist))))
+   (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+  (mapc #'(lambda (var)
+      (set (car var) (cadr var)))
+        account-vars)
+      (error "No email account found"))))
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
+
+;;----------------- Email setup ends --------------------
+
+;;----------------- Python IDE setup --------------------
+
+(prelude-require-packages '(elpy py-autopep8 epc jedi))
+(elpy-enable)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+(setq elpy-rpc-python-command "python3")
+
+;; Global Jedi config vars
+
+(defvar jedi-config:use-system-python nil
+  "Will use system python and active environment for Jedi server.
+May be necessary for some GUI environments (e.g., Mac OS X)")
+
+(defvar jedi-config:with-virtualenv nil
+  "Set to non-nil to point to a particular virtualenv.")
+
+(defvar jedi-config:vcs-root-sentinel ".git")
+
+(defvar jedi-config:python-module-sentinel "__init__.py")
+
+;; Helper functions
+
+;; Small helper to scrape text from shell output
+(defun get-shell-output (cmd)
+  (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string cmd)))
+
+;; Ensure that PATH is taken from shell
+;; Necessary on some environments without virtualenv
+;; Taken from: http://stackoverflow.com/questions/8606954/path-and-exec-path-set-but-emacs-does-not-find-executable
+
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell."
+  (interactive)
+  (let ((path-from-shell (get-shell-output "$SHELL --login -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+;; Package specific initialization
+(add-hook
+ 'after-init-hook
+ '(lambda ()
+
+    ;; Looks like you need Emacs 24 for projectile
+    (unless (< emacs-major-version 24)
+      (require 'projectile)
+      (projectile-global-mode))
+
+    ;; Auto-complete
+    (require 'auto-complete-config)
+    (ac-config-default)
+
+    ;; Uncomment next line if you like the menu right away
+    ;; (setq ac-show-menu-immediately-on-auto-complete t)
+
+    ;; Can also express in terms of ac-delay var, e.g.:
+    ;;   (setq ac-auto-show-menu (* ac-delay 2))
+
+    ;; Jedi
+    (require 'jedi)
+
+    ;; (Many) config helpers follow
+
+    ;; Alternative methods of finding the current project root
+    ;; Method 1: basic
+    (defun get-project-root (buf repo-file &optional init-file)
+      "Just uses the vc-find-root function to figure out the project root.
+       Won't always work for some directory layouts."
+      (let* ((buf-dir (expand-file-name (file-name-directory (buffer-file-name buf))))
+	     (project-root (vc-find-root buf-dir repo-file)))
+	(if project-root
+	    (expand-file-name project-root)
+	  nil)))
+
+    ;; Method 2: slightly more robust
+    (defun get-project-root-with-file (buf repo-file &optional init-file)
+      "Guesses that the python root is the less 'deep' of either:
+         -- the root directory of the repository, or
+         -- the directory before the first directory after the root
+            having the init-file file (e.g., '__init__.py'."
+
+      ;; make list of directories from root, removing empty
+      (defun make-dir-list (path)
+        (delq nil (mapcar (lambda (x) (and (not (string= x "")) x))
+                          (split-string path "/"))))
+      ;; convert a list of directories to a path starting at "/"
+      (defun dir-list-to-path (dirs)
+        (mapconcat 'identity (cons "" dirs) "/"))
+      ;; a little something to try to find the "best" root directory
+      (defun try-find-best-root (base-dir buffer-dir current)
+        (cond
+         (base-dir ;; traverse until we reach the base
+          (try-find-best-root (cdr base-dir) (cdr buffer-dir)
+                              (append current (list (car buffer-dir)))))
+
+         (buffer-dir ;; try until we hit the current directory
+          (let* ((next-dir (append current (list (car buffer-dir))))
+                 (file-file (concat (dir-list-to-path next-dir) "/" init-file)))
+            (if (file-exists-p file-file)
+                (dir-list-to-path current)
+              (try-find-best-root nil (cdr buffer-dir) next-dir))))
+
+         (t nil)))
+
+      (let* ((buffer-dir (expand-file-name (file-name-directory (buffer-file-name buf))))
+             (vc-root-dir (vc-find-root buffer-dir repo-file)))
+        (if (and init-file vc-root-dir)
+            (try-find-best-root
+             (make-dir-list (expand-file-name vc-root-dir))
+             (make-dir-list buffer-dir)
+             '())
+          vc-root-dir))) ;; default to vc root if init file not given
+
+    ;; Set this variable to find project root
+    (defvar jedi-config:find-root-function 'get-project-root-with-file)
+
+    (defun current-buffer-project-root ()
+      (funcall jedi-config:find-root-function
+               (current-buffer)
+               jedi-config:vcs-root-sentinel
+               jedi-config:python-module-sentinel))
+
+    (defun jedi-config:setup-server-args ()
+      ;; little helper macro for building the arglist
+      (defmacro add-args (arg-list arg-name arg-value)
+        `(setq ,arg-list (append ,arg-list (list ,arg-name ,arg-value))))
+      ;; and now define the args
+      (let ((project-root (current-buffer-project-root)))
+
+        (make-local-variable 'jedi:server-args)
+
+        (when project-root
+          (message (format "Adding system path: %s" project-root))
+          (add-args jedi:server-args "--sys-path" project-root))
+
+        (when jedi-config:with-virtualenv
+          (message (format "Adding virtualenv: %s" jedi-config:with-virtualenv))
+          (add-args jedi:server-args "--virtual-env" jedi-config:with-virtualenv))))
+
+    ;; Use system python
+    (defun jedi-config:set-python-executable ()
+      (set-exec-path-from-shell-PATH)
+      (make-local-variable 'jedi:server-command)
+      (set 'jedi:server-command
+           (list (executable-find "python3") ;; may need help if running from GUI
+                 (cadr default-jedi-server-command))))
+
+    ;; Now hook everything up
+    ;; Hook up to autocomplete
+    (add-to-list 'ac-sources 'ac-source-jedi-direct)
+
+    ;; Enable Jedi setup on mode start
+    (add-hook 'python-mode-hook 'jedi:setup)
+
+    ;; Buffer-specific server options
+    (add-hook 'python-mode-hook
+              'jedi-config:setup-server-args)
+    (when jedi-config:use-system-python
+      (add-hook 'python-mode-hook
+                'jedi-config:set-python-executable))
+
+    ;; And custom keybindings
+    (defun jedi-config:setup-keys ()
+      (local-set-key (kbd "M-.") 'jedi:goto-definition)
+      (local-set-key (kbd "M-,") 'jedi:goto-definition-pop-marker)
+      (local-set-key (kbd "M-?") 'jedi:show-doc)
+      (local-set-key (kbd "M-/") 'jedi:get-in-function-call))
+
+    ;; Don't let tooltip show up automatically
+    (setq jedi:get-in-function-call-delay 10000000)
+    ;; Start completion at method dot
+    (setq jedi:complete-on-dot t)
+    ;; Use custom keybinds
+    (add-hook 'python-mode-hook 'jedi-config:setup-keys)
+
+    ))
+
+(defun annotate-pdb ()
+  (interactive)
+  (highlight-lines-matching-regexp "import ipdb")
+  (highlight-lines-matching-regexp "ipdb.set_trace()"))
+(add-hook 'python-mode-hook 'annotate-pdb)
+(defun python-add-breakpoint ()
+  "Add a break point"
+  (interactive)
+  (newline-and-indent)
+  (insert "import ipdb; ipdb.set_trace()")
+  (highlight-lines-matching-regexp "^[ ]*import ipdb; ipdb.set_trace()"))
+
+;;----------------- Python setup end ------------
+
+;; -------------- multi term config -------------
+(prelude-require-package 'multi-term)
+(setq multi-term-program "/bin/zsh")
+
+(global-set-key (kbd "C-c T") 'multi-term)
+(global-set-key (kbd "C-c t") 'multi-term-next)
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq term-buffer-maximum-size 10000)
+            (setq show-trailing-whitespace nil)
+            (linum-mode -1)
+            ;;(autopair-mode -1)
+            (define-key term-raw-map (kbd "C-y") 'term-paste)
+            (add-to-list 'term-bind-key-alist '("C-c p" . multi-term-prev))
+            (add-to-list 'term-bind-key-alist '("C-c n" . multi-term-next))))
+
+(defcustom term-unbind-key-list
+  '("C-z" "C-x" "C-c" "C-h" "C-y" "<ESC>")
+  "The key list that will need to be unbind."
+  :type 'list
+  :group 'multi-term)
+
+(defcustom term-bind-key-alist
+  '(
+    ("C-c C-c" . term-interrupt-subjob)
+    ("C-p" . previous-line)
+    ("C-n" . next-line)
+    ("C-e" . end-line)
+    ("C-a" . beginning-of-line)
+    ("C-s" . isearch-forward)
+    ("C-r" . isearch-backward)
+    ("C-m" . term-send-raw)
+    ("M-f" . term-send-forward-word)
+    ("M-b" . term-send-backward-word)
+    ("M-o" . term-send-backspace)
+    ("M-p" . term-send-up)
+    ("M-n" . term-send-down)
+    ("M-M" . term-send-forward-kill-word)
+    ("M-N" . term-send-backward-kill-word)
+    ("M-r" . term-send-reverse-search-history)
+    ("M-," . term-send-input)
+    ("M-." . comint-dynamic-complete))
+  "The key alist that will need to be bind.
+  If you do not like default setup, modify it, with (KEY . COMMAND) format."
+  :type 'alist
+  :group 'multi-term)
+
+;; -------------- Multi term config ends  -------------
+
+
+;; --------- pdf config -------------
+(add-hook 'prog-mode-hook 'linum-on)
+(setq-default doc-view-pdfdraw-program "mudraw")
+;;-----------------------------------
